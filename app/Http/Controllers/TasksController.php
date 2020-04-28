50b35b238160e36c8d0bf2fa29fc960e57741e0e
hinako-m <muratani@lassic.co.jp>
2020-04-28 05:46:01 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -46,7 +46,14 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',   // 追加
+            'content' => 'required|max:191',
+        ]);
+        
         $task = new Task;
+        $task->string('status', 10);           //追加
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
         
@@ -94,7 +101,14 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',   // 追加
+            'content' => 'required|max:191',
+        ]);
+
         $task = Task::find($id);
+        $task->string('status', 10);           //追加
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
 
